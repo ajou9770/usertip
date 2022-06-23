2db89767d38d392ef44bd61ab4af261b94addec5
s.h kim <76585539+ajou9770@users.noreply.github.com>
2022-06-23 15:31:15 +0900

    img ch

--- videos.js
+++ videos.js
@@ -4,4 +4,4 @@ const chosenVideos = videos[Math.floor(Math.random() * videos.length)];
 
 const VideosFrame = document.querySelector("video");
 
-VideosFrame.src = `img/${chosenVideos}`;
+VideosFrame .src = `img/${chosenVideos}`;
